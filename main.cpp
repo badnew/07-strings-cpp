dd31e7e0b868481c8263593f0b561372cc7a60ba
badnew <badnew@mail.ru>
2017-06-19 22:36:56 +0300

    added

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <conio.h>
 
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    setlocale(LC_ALL, "Russian");
+
+    char str[80] = "\001Hello";
+
+    cout << "str: \"" << str << "\"\n";
+
+    _getch();
     return 0;
 }
